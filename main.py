244c3d7d1c9a9a7171f12f46d636772a314d2c7e
Josh Edralin <joshedralin@gmail.com>
2025-10-25 19:49:43 -0700

    CH2 L4: Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
-
+def count_words(string):
+    temp_list = string.split()
+    num_words = len(temp_list)
+    
+    return f"Found {num_words} total words"
+    
 def main():
-    print(get_book_text("books/frankenstein.txt"))
-
+    print(count_words(get_book_text("books/frankenstein.txt")))
 
 main()
