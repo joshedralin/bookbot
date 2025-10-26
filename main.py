15eae647838c02eda82274d184d6b01db9169231
Josh Edralin <joshedralin@gmail.com>
2025-10-25 19:40:50 -0700

    CH2, L3: Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        return f.read()
+
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+
+main()
