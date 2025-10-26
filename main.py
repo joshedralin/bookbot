a3acc6bd6b045f30d53d8052e406eec9b1c7b431
Josh Edralin <joshedralin@gmail.com>
2025-10-25 19:52:46 -0700

    CH2 L5: Add stats.py and refactor code

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
+from stats import get_num_words
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
-def count_words(string):
-    temp_list = string.split()
-    num_words = len(temp_list)
-    
-    return f"Found {num_words} total words"
-    
 def main():
-    print(count_words(get_book_text("books/frankenstein.txt")))
+    print(get_num_words(get_book_text("books/frankenstein.txt")))
 
 main()
---
+++ stats.py
@@ -0,0 +1,5 @@
+def get_num_words(string):
+    temp_list = string.split()
+    num_words = len(temp_list)
+    
+    return f"Found {num_words} total words"
