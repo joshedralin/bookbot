3aa5818d3314a05aacd4050292ae03e8d6ba9cbe
Josh Edralin <joshedralin@gmail.com>
2025-10-25 20:24:41 -0700

    CH3, L1: Update main and stats to include report

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from stats import get_num_words
 from stats import count_char
+from stats import sort_dict
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    file_path = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    book_text = get_book_text(file_path)
     print(get_num_words(book_text))
-    print(count_char(book_text))
+    print("--------- Character Count -------")
 
+    char_list = sort_dict(count_char(book_text))
+    for item in char_list:
+        if item["char"].isalpha():
+            print (f"{item["char"]}: {item["num"]}")
+
+    print("============= END ===============")
+    
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,17 @@ def count_char(string):
             char_dict[char] = 1
     
     return char_dict
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(char_dict):
+    char_list = []
+    
+    for item in char_dict:
+        value = char_dict[item]
+        char_list.append({"char": item, "num": value})
+        
+    char_list.sort(reverse=True, key=sort_on)
+    
+    return char_list
