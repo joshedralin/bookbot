b39ddff1c72206556f4e3d7fc68bf6178ef3a5b5
Josh Edralin <joshedralin@gmail.com>
2025-10-25 20:01:05 -0700

    CH2, L6: Update stats.py and main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from stats import get_num_words
+from stats import count_char
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
 def main():
-    print(get_num_words(get_book_text("books/frankenstein.txt")))
+    book_text = get_book_text("books/frankenstein.txt")
+    print(get_num_words(book_text))
+    print(count_char(book_text))
 
 main()
--- stats.py
+++ stats.py
@@ -3,3 +3,14 @@ def get_num_words(string):
     num_words = len(temp_list)
     
     return f"Found {num_words} total words"
+
+def count_char(string):
+    char_dict = {}
+    for c in string:
+        char = c.lower()
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    
+    return char_dict
